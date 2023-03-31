f8f2e3cc34cb2da4f82b28fd3b2af0cae99cd19e
shine <villavicencio.martin@pm.me>
2023-03-31 23:00:46 +0200

    Init more detailed app_information

--- rofi_semantic/app_information.py
+++ rofi_semantic/app_information.py
@@ -1,42 +1,51 @@
+from functools import reduce
 from ripgrepy import Ripgrepy
-from constants import APPLICATION_PATH
-import subprocess
+from os import path, environ
 
 
-def get_description():
-    rg = Ripgrepy('(^Exec=([\w-]*))|(^Name=.*)|(^GenericName=.*)', APPLICATION_PATH)
-    results = rg.only_matching().json().run().as_dict
+def get_app_paths():
+    paths = environ.get("XDG_DATA_DIRS").split(':')
+    paths = [p for p in paths if path.exists(p)]
+    return paths
 
-    matches = {}
-    for m in results:
-        file_path = m['data']['path']['text']
-        if file_path in matches:
-            matches[file_path].append(m['data']['submatches'])
-        else:
-            matches[file_path] = [(m['data']['submatches'])]
+
+def get_raw_app_information():
+    matches = []
+    for desktop_path in get_app_paths():
+        rg = Ripgrepy(r'Exec=([^\s\n]*)|Name=(.*)|GenericName=(.*)', desktop_path)
+        results = rg.glob('*.desktop').only_matching().json().run().as_dict
+        matches += results
     return matches
 
 
-def list_apps():
-    """List desktop apps in the APPLICATION_PATH folder.
+def map_match(match):
+    match_type, submatch = match['data']['submatches'][0]['match'].split('=')
+    return {
+            'path': match['data']['path'],
+            'type': match_type,
+            'match': submatch
+    }
 
-    Returns:
-        Array with desktop apps.
-    """
-    command = 'ls ' + APPLICATION_PATH
-    ls_output = subprocess.check_output(command, shell=True)
 
-    program_list = ls_output.decode().split('\n')
+def reduce_matches(match_list, new_match):
+    pass
 
-    return program_list
+
+def get_app_information():
+    matches = get_raw_app_information()
+    matches = map(map_match, matches)
+    matches = reduce(reduce_matches, matches, [])
 
 
 def list_app_names():
+    pass
+    """
     program_list = list_apps()
     # remove desktop postfix
     program_list = [app_basename.replace('.desktop', '') for app_basename in program_list]
     return program_list
+    """
 
 
 if __name__ == '__main__':
-    get_description()
+    get_app_information()
